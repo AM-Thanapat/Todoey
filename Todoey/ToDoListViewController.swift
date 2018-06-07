27162e14669c6d6c07312798bce3a4a2de7072ac
Thanapat Saisoontornwattana <thanapatsaisoontornwattana@khwans-mac-mini.local>
2018-06-07 15:19:23 +0700

    add and implement addToDoButton to update tableview

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["aa", "bb", "cc"]
+    var itemArray = ["aa", "bb", "cc"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -54,6 +54,23 @@ class ToDoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func addButtonPress(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if textField.text != ""{
+                self.itemArray.append(textField.text!)                
+            }
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     /*
     // Override to support conditional editing of the table view.
